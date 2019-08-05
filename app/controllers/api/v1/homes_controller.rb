ee5433d5c4e74dc6a48ea724e0131bc9975fc7b8
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-08-05 13:47:02 +0100

    User and Home relationships

--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::HomesController < ApplicationController
+    def create
+        home = Home.create(home_params)
+        # home.user = @current_user
+        # if home.valid?
+        #     home.save
+        #     render json: home, status: :created
+        # else
+        #     render json: { errors: home.errors.full_messages }, status: :not_accepted
+        # end
+    end
+
+    private
+
+    def home_params
+        params.require(:post).permit(:name, :address_one, :address_two, :city, :postcode, :home_key)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Home.create(name: 'House one', address_one: '6', address_two: 'Hurst Avenue', city: 'London', postcode: 'E4 8DW', home_key: '1')
+# User.create(email:'vlad.georgiev@outlook.com',password:'1', first_name: 'Vlad', last_name: 'Georgiev', home_id: 1)
