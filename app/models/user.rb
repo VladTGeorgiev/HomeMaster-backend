0319fe94fbfb58e3839586497dad2405b5691ed0
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-08-06 11:03:12 +0100

    Added swal

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   belongs_to :home
   has_secure_password
+  # before_create :default_image
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  # def default_image
+  #   self.avatar.attach(io: File.open(Rails.root.join('app','assets', 'images', 'default_image.png')), filename: 'default-image.png', content_type: 'image/png')
+  # end
 end
